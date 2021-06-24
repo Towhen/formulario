b5762acc96d8356b5622ecc607ddfdbfd38355fc
Saul <kendalo27@hotmail.com>
2021-06-24 00:01:38 -0400

    nueva version

--- lib/db/operation.dart
+++ lib/db/operation.dart
@@ -1,8 +1,19 @@
+import 'package:flutter_app_cuestonario/models/note.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class Operation {
-  Future<Database> openDB() async {
-    return openDatabase(join(await getDatabasesPath(), 'note.db'));
+  static Future<Database> _openDB() async {
+    return openDatabase(join(await getDatabasesPath(), 'note.db'),
+        onCreate: (db, version) {
+      return db.execute(
+          "CREATE TABLE notes (id INTEGER PRIMARY KEY, title TEXT, content TEXT)");
+    }, version: 1);
+  }
+
+  static Future<void> insert(Note note) async {
+    Database database = await _openDB();
+
+    return database.insert("notes", note.toMap());
   }
 }
